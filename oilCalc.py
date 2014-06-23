b8fecb880ef416fb6a6fdfa063fc1bd93e079a56
chalkie666 <dan@chalkie.org.uk>
2014-06-23 14:12:00 +0200

    version 0.2
    fleshed out description regarding straight path only,
    more output of input data,
    actual WD is that given plus coverslip thickness
    input data all float
    refined input data values from internet sources given
    seems to give answers that deviate from the design RI of oil
    at least in the right direction. So equation might be ok as specified.

--- oilCalc.py
+++ oilCalc.py
@@ -21,7 +21,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# this script calculates the refractive index of immersion oil needed 
+# this script attempt to calculate the refractive index of immersion oil needed 
 # to correct out spherical aberrations caused by the 
 # early or late arrival (compared to the design specification of an objective lens) 
 # of photons going in a straight line from an object a few microns deep embedded in 
@@ -31,7 +31,19 @@
 # minus the cover glass thickness and object depth into the mounting medium, and we
 # assume its the same in both the designed and aberrated cases. Not sure thats right
 # and we also make no mention of numerical aperture of the lens, which is probably
-# an important detail, or does it end up being the same due to trigonometry?
+# an important detail. Or does it end up being the same due to trigonometry?
+# 
+# Rather than using fancy physics and complex equations to work this out, we take a
+# simple approach of assuming the spherical aberration is caused by the difference in 
+# optical path in ideal designed scenario vs. real abberated situations. 
+# (optical path length is the distance the photon "feels" it went, 
+# which is just a clever way of saying the time it takes the photon to travel
+# dependent on the real distance and the regractive indices of the stuff photons go
+# through - that is to say the different speed of light in different media) 
+# We only consider photons travelling is a straight line from the object emitting
+# light to the lens front surface. 
+# So far we make no calculations about photons going in other directions, 
+# which is perhaps required. So this might all be totally wrong!!! 
 
 # equations 
 
@@ -79,24 +91,26 @@
 
 # distances in micrometers, refractive index is dimensionless
 
-RIoilneeded          =  0    # refractive index of immersion oil needed co cpmpensate for the sample, the goal
-objDepth             =  0   # distance of object emitting light, from the coverslip / mounting media interface. 
-RIsampledes          =  1.52 # refractive index of the sample and/or mounting medium, according to lens design, eg 1.52
-covSlipThickdes      =  170  # cover slip glass thickness by lens design, typically 170 micrometers
-RIcsdes              =  1.52 # refractive index of coverslip in lens design, eg 1.52
-WD                   =  3000 # lens working distance - lens dependent, look it up on the manufactuer website
-RIoildes             =  1.515# refractive index of immersion oil used in lens design, as provided by manufacturer og the lens. 
-RIsamplereal         =  1.334# real refractive index of the sample and/or mounting medium, eg water is 1.33, glycerol 1.47
-covSlipThickreal     =  170  # real coverslip thickness, hopefully exactly 170 micrometers, might be something else.
-RIcsreal             =  1.52 # real refractive index of the coverslip glass, eg 1.52
+RIoilneeded          =  0.00   # refractive index of immersion oil needed compensate for the sample: the goal!
+objDepth             =  0.00   # distance of object emitting light, from the coverslip / mounting media interface. 
+RIsampledes          =  1.5255 # refractive index of the sample and/or mounting medium, according to lens design, eg 1.52
+covSlipThickdes      =  170.0  # cover slip glass thickness by lens design, typically 170 micrometers
+RIcsdes              =  1.5255 # refractive index of coverslip in lens design, eg 1.52, 1.5255 plus minus 0.0015 at 541 nm, or 1.5230 at 589.3 nm, Abbe value, ve 55.from http://www.menzel.de/fileadmin/Templates/Menzel/pdf/en/Produktinfo_Deckglas_englisch_01.pdf
+WD                   =  (150.0+covSlipThickdes)  # lens working distance - lens dependent, look it up on the manufactuer website, eg http://microscope.olympus-global.com/uis2/en/plapon60xo/
+RIoildes             =  1.515  # refractive index of immersion oil used in lens design, as provided by manufacturer of the lens. Olympus oil for 60x1.42 is 1.515. 
+RIsamplereal         =  1.47   # real refractive index of the sample and/or mounting medium, eg water is 1.33, glycerol 1.47
+covSlipThickreal     =  130.0  # real coverslip thickness, hopefully exactly 170 micrometers, might be something else.
+RIcsreal             =  1.5255 # real refractive index of the coverslip glass, eg 1.52
 
-#the equation to calculate the refractive index of the immersion oil 
+# the equation to calculate the refractive index of the immersion oil 
 # to compensate for the spherical aberration caused by the sample.
 
 RIoilneeded = ( (objDepth*RIsampledes) + (covSlipThickdes*RIcsdes) + ((WD-objDepth-covSlipThickdes)*RIoildes) - (objDepth*RIsamplereal) - (covSlipThickreal*RIcsreal) ) / (WD-objDepth-covSlipThickreal)
 
-print RIoilneeded
-
+print "RI of immersion oil to correct SA is " + str(RIoilneeded)
+print "object depth is " + str(objDepth)
+print "RI of sample is "+ str(RIsamplereal)
+print "real coverslip thickness is "+ str(covSlipThickreal)
 
 
 
